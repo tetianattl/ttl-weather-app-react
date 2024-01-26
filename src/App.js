37d61abaf59802132e7d9699ba0be68ca0c03075
tetianattl <tetyana.ttl@gmail.com>
2024-01-26 16:15:18 +0200

    added Github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Tetiana Tiukhta and is open-sourced on{" "}
+        <a
+          href="https://github.com/tetianattl/ttl-weather-app-react"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
