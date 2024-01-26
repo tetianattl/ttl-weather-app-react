bc6e6dc0dc0f641ca2dd0c93abbeec0fbe40cecb
tetianattl <tetyana.ttl@gmail.com>
2024-01-26 17:21:06 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "./App.css";
 
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Tetiana Tiukhta and is open-sourced on{" "}
-        <a
-          href="https://github.com/tetianattl/ttl-weather-app-react"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Tetiana Tiukhta and is open-sourced on{" "}
+          <a
+            href="https://github.com/tetianattl/ttl-weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
